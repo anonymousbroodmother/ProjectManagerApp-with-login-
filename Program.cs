cc954c734b699c3f758983b41ff1641e03538252
anonymousbroodmother <zdengin34@gmail.com>
2025-10-30 14:56:34 +0400

    login

--- Program.cs
+++ Program.cs
@@ -1,17 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
 namespace WinFormsApp1
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false); 
+
+            var loginForm = new LoginForm();
+            if (loginForm.ShowDialog() == DialogResult.OK)
+            {
+                Application.Run(new Form1());
+            }
+            else
+            {
+                Application.Exit();
+            }
         }
     }
 }
